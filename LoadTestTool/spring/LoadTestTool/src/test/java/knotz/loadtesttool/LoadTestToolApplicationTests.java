a0d8e4224cf5c305a2efa27bc61829895114e147
김한결 <kmr5326@naver.com>
2025-01-06 17:06:17 +0900

    프로젝트 생성

--- LoadTestTool/spring/LoadTestTool/src/test/java/knotz/loadtesttool/LoadTestToolApplicationTests.java
+++ LoadTestTool/spring/LoadTestTool/src/test/java/knotz/loadtesttool/LoadTestToolApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package knotz.loadtesttool;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class LoadTestToolApplicationTests {
 
 	@Test
 	void contextLoads() {
