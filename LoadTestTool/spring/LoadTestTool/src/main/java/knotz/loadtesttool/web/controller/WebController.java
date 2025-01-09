284b515402712a4594fc18d5c16c98ba88eef737
김한결 <kmr5326@naver.com>
2025-01-09 14:41:36 +0900

    chore: ãweb 디렉토리 생성

--- LoadTestTool/spring/LoadTestTool/src/main/java/knotz/loadtesttool/web/controller/WebController.java
+++ LoadTestTool/spring/LoadTestTool/src/main/java/knotz/loadtesttool/web/controller/WebController.java
@@ -1,4 +1,4 @@
-package knotz.loadtesttool.controller;
+package knotz.loadtesttool.web.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
